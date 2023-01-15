9ca76682cbb502677e4e852d900ce8d179f231cd
Lucas Picca <luksismael@gmail.com>
2023-01-15 11:00:31 -0300

    subo clase1

---
+++ clase1/app1.js
@@ -0,0 +1,71 @@
+/* let n1 = parseInt(prompt("ingrese un número"));
+let n2 = parseInt(prompt("ingrese otro número"));
+
+let resultado = alert(n1+n2); */
+
+//Ejercicio 2
+/* let n1 = parseInt(prompt("ingrese un número y se elevará al cubo"));
+
+let resultado = alert(n1*n1*n1); */
+
+//Ejercicio 3
+/* let año1 = parseInt(prompt("Ingrese el año actual"));
+let año2 = parseInt(prompt("Ingrese el año de su nacimiento"));
+
+let resultado = alert(año1 - año2); */
+
+//Ejercicio 4
+/* let km = parseInt(prompt("Ingrese la distancia en km entre dos ciudades"));
+let velocidad = parseInt(prompt("Ingrese la velocidad promedio del vehículo en km/h"));
+
+let tiempo = alert("El tiempo que tarda es: " + km/velocidad); */
+
+//Ejercicio 5
+/* let tFact = parseInt(prompt("Ingrese el total facturado del empleado1"));
+
+let sueldo = alert("El sueldo de empleado 1 es: " + (((tFact *5)/100) + 15000)) */
+
+//Ejercicio 6
+/* let nota1 = parseInt(prompt("Ingrese una nota"));
+let nota2 = parseInt(prompt("Ingrese otra nota"));
+let nota3 = parseInt(prompt("Ingrese otra nota"));
+
+let promedio = (nota1 + nota2 + nota3)/3;
+
+alert("Su promedio es: " + promedio) */
+
+//Ejercicio 7
+/* let mt = parseInt(prompt("Ingrese metros cuadrados totales"));
+let mc = parseInt(prompt("Ingrese metros cuadrados cubiertos"));
+
+let md = mt - mc;
+let pmc = mc * 100/mt;
+let pmd = md * 100/mt;
+
+alert("El promedio de metros cuadrados cubiertos es: " + pmc + "\nEl promedio de metros cuadrados descubiertos es: " + pmd) */
+
+//Ejercicio 8
+/* let mt = parseInt(prompt("Ingrese el monto total de su compra"));
+let descuento = mt * 0.85;
+
+alert("El descuento aplicado de su compra es: " + descuento) */
+
+//Ejercicio 9
+/* let m = parseInt(prompt("Ingrese total de mujeres"));
+let h = parseInt(prompt("Ingrese total de hombres"));
+let t = m + h;
+let pm = m * 100/t;
+let ph = h * 100/t;
+
+alert("El promedio de mujeres es: " + pm + "\nEl promedio de hombres es: " + ph) */
+
+//Ejercicio 10
+let n1 = parseInt(prompt("Ingrese un número"));
+let n2 = parseInt(prompt("Ingrese otro número"));
+
+let s = n1 + n2;
+let r = n1 - n2;
+let m = n1 * n2;
+let d = n1/n2 ;
+
+alert(s + " " + r + " " + m + " " + d)
